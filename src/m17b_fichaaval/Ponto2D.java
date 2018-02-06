778f81051c468dd9c4f790865bb063214c89823e
a16972 <a16972@d1-05-12.dped.local>
2018-02-06 15:17:34 +0000

    Versão 1.1

--- src/m17b_fichaaval/Ponto2D.java
+++ src/m17b_fichaaval/Ponto2D.java
@@ -8,6 +8,22 @@ public class Ponto2D {
         x = 0;
         y = 0;        
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
     
     public Ponto2D(int x, int y){
         this.x = x;
